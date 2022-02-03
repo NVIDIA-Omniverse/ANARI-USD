3fe34793af48622616ac0b2d8b49d3e60e28c367
Kees Van Kooten <kvankooten@nvidia.com>
2022-02-03 19:56:04 +0100

    Modulus branch uses single-stage USD output with timestep support

--- UsdBridge/UsdBridgeMacros.h
+++ UsdBridge/UsdBridgeMacros.h
@@ -7,11 +7,11 @@
 #define SUPPORT_MDL_SHADERS
 
 #ifdef TIME_BASED_CACHING
-#define VALUE_CLIP_RETIMING // Retiming of timesteps through value clips, for selected objects
+//#define VALUE_CLIP_RETIMING // Retiming of timesteps through value clips, for selected objects
 #endif
 
 #ifdef VALUE_CLIP_RETIMING
-#define TIME_CLIP_STAGES // Separate clip stages for each timestep, for selected objects
+//#define TIME_CLIP_STAGES // Separate clip stages for each timestep, for selected objects
 #endif
 
 #define USE_USD_GEOM_POINTS
